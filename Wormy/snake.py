66e21fcc4695a5743350baae485e898cdeb0b266
ForteGS <minh.forte@gmail.com>
2016-03-07 11:21:27 -0600

    Finish adding moving animation for wormy.

--- Wormy/snake.py
+++ Wormy/snake.py
@@ -8,11 +8,16 @@ from pygame.locals import *
 POINTSIZE = 10
 WINDOWSWIDTH = 640
 WINDOWSHEIGHT = 640
-FPS = 30
+FPS = 60
 BASICFONTSIZE = 20
 TITLEFONTSIZE = 120
 INITSNAKELEN = 4
 BORDERSIZE = WINDOWSHEIGHT / POINTSIZE - POINTSIZE
+
+UP = 'up'
+DOWN = 'down'
+RIGHT = 'right'
+LEFT = 'left'
 
 # RGB
 WHITE = (255, 255, 255)
@@ -25,12 +30,14 @@ BORDERCOLOR = BLUE
 SYSTEMTEXTSCOLOR = WHITE
 POINTCOLOR = WHITE
 TITLETEXTCOLOR = RED
+BGCOLOR = BLACK
 
 
 def main():
-    global BASICFONT, TITLEFONT, DISPLAYSURF, MENUSURF, GAMESURF, TOPSCORESURF, INPUTSURF
+    global BASICFONT, TITLEFONT, DISPLAYSURF, MENUSURF, GAMESURF, TOPSCORESURF, INPUTSURF, FPSCLOCK
 
     pygame.init()
+    FPSCLOCK = pygame.time.Clock()
     MENUSURF = pygame.display.set_mode((WINDOWSWIDTH, WINDOWSHEIGHT))
     GAMESURF = MENUSURF.copy()
     TOPSCORESURF = MENUSURF.copy()
@@ -63,11 +70,13 @@ def main():
     # Main game components: Wormy, border, system text message, 
     # scores, and food.
     gameObj = []
-    drawBorder()
+    #drawBorder()
+
+    direction = 0
+    gameStart = False
 
     # Draw menu object.
     drawOnSurface(MENUSURF, menuObj)
-
     # Main game loop.
     while True:
         checkForQuit()
@@ -76,22 +85,39 @@ def main():
                 # Clicked on start button.
                 if startRect.collidepoint(event.pos):
                     MENUSURF.blit(GAMESURF, (0, 0))
-                    wormy, (foodx, foody) = initStateGame()
+                    wormy, foods = initStateGame()
 
                     # Draw wormy and food.
                     for x, y in wormy:
                         drawPoint(x, y)
 
-                    drawPoint(foodx, foody)
+                    for foodx, foody in foods:
+                        drawPoint(foodx, foody)
 
+                    gameStart = True
 
+                    # Detecting input keyboard for changing direction.
 
                 # View the top scores.
                 elif topScoreRect.collidepoint(event.pos):
                     MENUSURF.blit()
                     return
 
+            elif event.type == KEYUP:
+                if event.key in (K_LEFT, K_a):
+                    direction = LEFT
+                elif event.key in (K_RIGHT, K_d):
+                    direction = RIGHT
+                elif event.key in (K_UP, K_w):
+                    direction = UP
+                elif event.key in (K_DOWN, K_s):
+                    direction = DOWN
+
+        if direction and gameStart:
+            moveUpdate(wormy, direction)
+
         pygame.display.update()
+        FPSCLOCK.tick(FPS)
 
 def make_text(font, text, color, center):
     textSurf = font.render(text, True, color)
@@ -117,13 +143,14 @@ def checkForQuit():
 
 def drawBorder(message = None):
     pygame.draw.rect(GAMESURF, BLUE, \
-            (POINTSIZE - 1, POINTSIZE - 1, POINTSIZE * BORDERSIZE + 1, POINTSIZE * BORDERSIZE + 1), 1)
+            (POINTSIZE - 2, POINTSIZE - 2, POINTSIZE * BORDERSIZE + 1, POINTSIZE * BORDERSIZE + 1), 1)
     return
 
 def initStateGame():
     # There should be a list of point that belongs to the snake.
     # The first point in the list belongs to the snake.
     wormy = []
+    foods = []
     for i in range (INITSNAKELEN):
         x, y = getTopLeft((BORDERSIZE + INITSNAKELEN) / 2 + i * POINTSIZE,\
                 (BORDERSIZE + INITSNAKELEN) / 2)
@@ -133,8 +160,9 @@ def initStateGame():
     # Have to consider the border line too.
     # The food cannot be outside the border line.
     foodx, foody = placeRandFood(wormy)
+    foods.append((foodx, foody))
 
-    return wormy, (foodx, foody)
+    return wormy, foods
 
 def placeRandFood(wormy):
     foodx, foody = getTopLeft(random.randint(POINTSIZE + 1, POINTSIZE * BORDERSIZE - 1),\
@@ -152,6 +180,40 @@ def drawPoint(x, y):
     pygame.draw.rect(MENUSURF, POINTCOLOR, (x, y, POINTSIZE, POINTSIZE))
     return
 
+def moveUpdate(wormy, direction):
+    # This function does not check valid move.
+
+    # Assign to the head first.
+    oldhead = wormy[len(wormy) - 1]
+
+    oldTail = wormy[0]
+
+    # Update the body's and tail's coordinates.
+    for i in range(len(wormy) - 1):
+        wormy[i] = wormy[i + 1]
+
+    # Update the head's coordinates.
+    if direction == UP:
+        wormy[len(wormy) - 1] = getTopLeft(oldhead[0], oldhead[1] - POINTSIZE)
+    elif direction == DOWN:
+        wormy[len(wormy) - 1] = getTopLeft(oldhead[0], oldhead[1] + POINTSIZE)
+    elif direction == LEFT:
+        wormy[len(wormy) - 1] = getTopLeft(oldhead[0] - POINTSIZE, oldhead[1])
+    elif direction == RIGHT:
+        wormy[len(wormy) - 1] = getTopLeft(oldhead[0] + POINTSIZE, oldhead[1])
+
+    baseSurf = MENUSURF.copy()
+
+    # Draw a blank space over the moving point on thte baseSurf Surface.
+    moveLeft, moveTop = getTopLeft(oldTail[0], oldTail[1])
+    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, POINTSIZE, POINTSIZE))
+
+    for i in range(len(wormy)):
+        checkForQuit()
+        MENUSURF.blit(baseSurf, (0, 0))
+        drawPoint(wormy[i][0], wormy[i][1])
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
 
 if __name__ == '__main__':
     main()
